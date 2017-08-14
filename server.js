4dacdce4a5b06dca2c33e53cbd534a04dbd4e81a
vickybipul <vickybipulroy@gmail.com>
2017-08-15 02:32:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+  res.sendFile(path.join(__dirname, 'ui', 'ok.html'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
